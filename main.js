e65afebac46c29a7ea1106e092e2c85ac6d0d353
朱修毅 <qmore0305@gmail.com>
2022-07-06 23:08:48 +0800

    nightver

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ function del(obj){
     let ttable = document.getElementById("table")
     ttable.deleteRow(inde);
 }
+function edit(obj){
+    var inde = obj.parentNode.parentNode.rowIndex;
+    let ttable = document.getElementById("table")
+    ttable.deleteRow(inde)
+}
 
 function addRow(){
-    if(time.value=='' && place.value=='' || description.value==''){
-        if(description.value==''){
-            alert("請至少要輸入描述")
-        }
-        else{
-            alert("請輸入內容")
-        }
+    if(description.value==''){
+        alert("請至少要輸入描述");
         return;
     }     
-    tabody.innerHTML += "<tr><td>"+time.value+"</td><td>"+place.value+"</td><td>"+description.value+"</td><td><button class='delbutton' type='button' onclick='del(this)'>刪除</button></td></tr>";
+    tabody.innerHTML += "<tr><td>"+time.value+"</td><td>"+place.value+"</td><td>"+description.value+"</td><td><button class='delbutton' type='button' onclick='edit(this)''>編輯</button><button class='delbutton' type='button' onclick='del(this)'>刪除</button></td></tr>";
     time.value = "";
     place.value = "";
     description.value = "";
