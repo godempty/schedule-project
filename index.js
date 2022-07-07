08550a85376b82bd054c6715daa8f537a737e45c
朱修毅 <qmore0305@gmail.com>
2022-07-08 00:38:01 +0800

    database so hard Orz

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT
+    ,{useNewUrlParser: true}
+    , () => console.log('db connected!')
+);
+
+
+app.listen(5000, () => console.log('server running'));
