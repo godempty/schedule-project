a2849b0abc863ad45756ac54c61404f64b8f65f3
朱修毅 <qmore0305@gmail.com>
2022-09-23 00:34:11 +0800

    database and animation and some preference

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 from flask import Flask, redirect, render_template, request, url_for
 import pymongo
+from bson.objectid import ObjectId
 app = Flask(__name__)
 true = True
-myclient = pymongo.MongoClient('mongodb+srv://godempty:momo0305@cluster0.xshvh.mongodb.net/?retryWrites=true&w=majority',tls=True,tlsAllowInvalidCertificates=True)
+myclient = pymongo.MongoClient('mongodb+srv://godempty:momo0305@cluster0.i5qta6c.mongodb.net/?retryWrites=true&w=majority',tls=True,tlsAllowInvalidCertificates=True)
 mydb = myclient['Schedule']
-mycollection = mydb['collection']
+datadb = mydb['datas']
 
 @app.route('/schedule', methods=['POST','GET'])
 def show():
-    return render_template("index.html")
-@app.route('/update', methods=['POST','GET'])
-def update():
-    print(request)
-    print(request.data)
-    print(request.json)
-    return render_template("index.html")
+    if request.method == 'POST':
+        newdata = {
+            'time': request.form.get('time'),
+            'place': request.form.get('place'),
+            'description': request.form.get('description')
+        }
+        datadb.insert_one(newdata)
+    datas = datadb.find()
+    return render_template("normal.html", datas=datas)
+@app.route('/delete/<id>')
+def delete(id):
+    datadb.delete_one({'_id': ObjectId(id)})
+    return redirect('/schedule')
 
 if __name__ == "__main__":
     app.run(port='3060', debug=True)
